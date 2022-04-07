2411a0c5fb606e82ddbf0a51cb05db41029a3916
MrFogg866 <philiplynch866@gmail.com>
2022-04-07 15:18:50 +0000

    starting to add javascript - constant and maths

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
 console.log("Connected")
+
+const options = document.querySelectorAll(".options");
+let pScore = 0;
+let cScore = 0;
+
+options.forEach((option) => {
+    option.addEventListener("click", funtion () {
+        const pInput = this.value;
+
+        const cOptions = ["Cannonball", "Map", "Swords"];
+        const cInput = cOptions[Math.floor(Math,random() * 3)];
+
+        updateMoves(pInput, cInput);
+        compareInputs(pInput, cInput);
+        updateScore();
+        if(checkWinner()){
+            pScore = cScore= 0;
+            updateScore();
+        }
+    });
+});
