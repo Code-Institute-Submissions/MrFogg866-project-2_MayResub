c1429bb2aee2f5116fe39cd6622f218698d55609
MrFogg866 <philiplynch866@gmail.com>
2022-05-22 15:41:01 +0000

    run the code through the js beautifier

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
-
-
 const options = document.querySelectorAll(".options");
 let pTally = 0;
 let cTally = 0;
 
 options.forEach((option) => {
-    option.addEventListener("click", function () {
+    option.addEventListener("click", function() {
         const pInput = this.value;
 
         const cOptions = ["Cannonball", "Map", "Swords"];
@@ -14,80 +12,78 @@ options.forEach((option) => {
         updateMoves(pInput, cInput);
         compareInputs(pInput, cInput);
         updateTally();
-        if(checkWinner()){
-            pTally = cTally= 0;
+        if (checkWinner()) {
+            pTally = cTally = 0;
             updateTally();
         }
     });
 });
 
 function displayMessage(message) {
-  document.getElementById('display-message').innerHTML = message;
+    document.getElementById('display-message').innerHTML = message;
 
 }
 
 
-function updateMoves(pInput, cInput){
-   
+function updateMoves(pInput, cInput) {
+
     document.getElementById("p-move").src = `./assets/images/${pInput.toLowerCase()}.PNG`;
     document.getElementById("c-move").src = `./assets/images/${cInput.toLowerCase()}.PNG`;
-  }
-
-
-  function compareInputs(pInput, cInput) {
-    const currentMatch = `${pInput} vs ${cInput}`;
-    if (pInput === cInput) {
-      displayMessage(`${currentMatch} is a Tie`);
-      return;
-    }
-
-
-//Check for Cannonball
-if (pInput === "Cannonball") {
-    if (cInput === "Swords") {
-      alert(`${currentMatch} = You Win`);
-      pTally++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      cTally++;
-    }
-  }
-
-
- //Check for Map
- else if (pInput === "Map") {
-    if (cInput === "Cannonball") {
-      alert(`${currentMatch} = You Win`);
-      pTally++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      cTally++;
-    }
-  }
-
-  //Check for Swords
-  else {
-    if (cInput === "Map") {
-      alert(`${currentMatch} = You Win`);
-      pTally++;
-    } else {
-      alert(`${currentMatch} = Computer Wins`);
-      cTally++;
-    }
-  }
 }
 
+
+function compareInputs(pInput, cInput) {
+    const currentMatch = `${pInput} vs ${cInput}`;
+    if (pInput === cInput) {
+        displayMessage(`${currentMatch} is a Tie`);
+        return;
+    }
+
+
+    //Check for Cannonball
+    if (pInput === "Cannonball") {
+        if (cInput === "Swords") {
+            alert(`${currentMatch} = You Win`);
+            pTally++;
+        } else {
+            alert(`${currentMatch} = Computer Wins`);
+            cTally++;
+        }
+    }
+
+
+    //Check for Map
+    else if (pInput === "Map") {
+        if (cInput === "Cannonball") {
+            alert(`${currentMatch} = You Win`);
+            pTally++;
+        } else {
+            alert(`${currentMatch} = Computer Wins`);
+            cTally++;
+        }
+    }
+
+    //Check for Swords
+    else {
+        if (cInput === "Map") {
+            alert(`${currentMatch} = You Win`);
+            pTally++;
+        } else {
+            alert(`${currentMatch} = Computer Wins`);
+            cTally++;
+        }
+    }
+}
+// Update player & computer tally
 function updateTally() {
     document.getElementById("p-tally").textContent = pTally;
     document.getElementById("c-tally").textContent = cTally;
-  }
+}
 //winner score and alert function
-  function checkWinner() {
+function checkWinner() {
     if (pTally === 3 || cTally === 3) {
-      const winner =
-        pTally === 3 ? "You win the game! Congratulations!" : "Computer wins the game! Try again next time!";
-      alert(winner);
-      return true;
+        const winner =
+            pTally === 3 ? "You win the game! Congratulations!" : "Computer wins the game! Try again next time!";
+        alert(winner);
+        return true;
     }
-    return false;
-  }
